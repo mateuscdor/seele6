1f0fb1094016d3d6f536f2bf2d0330077ef6c24f
synbee <97864273+synbee@users.noreply.github.com>
2022-07-31 13:01:29 +0530

    Update Info.ts

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -17,12 +17,11 @@ export default class extends BaseCommand {
         }
         const image = this.client.assets.get('whatsapp-bot') as Buffer
         const uptime = this.client.utils.formatSeconds(process.uptime())
-        const text = `🌟 *WhatsApp-bot* 🌟\n\n📙 *Description: ${description}*\n\n🔗 *Commands:* ${this.handler.commands.size}\n\n🚦 *Uptime:* ${uptime}`
+        const text = `*${description}*\n\n⚪ *Commands:* ${this.handler.commands.size}\t\t\t\t\⚪ *Uptime:* ${uptime}`
         return void (await reply(image, 'image', undefined, undefined, text, undefined, {
             title: this.client.utils.capitalize(name),
             thumbnail: image,
-            mediaType: 1,
-            sourceUrl: homepage
+            mediaType: 1
         }))
     }
 }
