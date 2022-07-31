febb199e8781b460987509f2bf7a1e6d15841798
synbee <97864273+synbee@users.noreply.github.com>
2022-07-31 13:05:28 +0530

    Update Sticker.ts

--- src/Commands/Utils/Sticker.ts
+++ src/Commands/Utils/Sticker.ts
@@ -28,8 +28,8 @@ export default class extends BaseCommand {
         const pack = context.split('|')
         const sticker = new Sticker(buffer, {
             categories,
-            pack: pack[1] ? pack[1].trim() : '🤍 Made for you',
-            author: pack[2] ? pack[2].trim() : `${this.client.config.name} 🖤`,
+            pack: pack[1] ? pack[1].trim() : '>.<',
+            author: pack[2] ? pack[2].trim() : `S E E L E`,
             quality,
             type:
                 flags.includes('--c') || flags.includes('--crop') || flags.includes('--cropped')
